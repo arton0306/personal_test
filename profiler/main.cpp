e25677687865f9f975dde19c3a903fa9fd588f77
Arton Chang <arton0306@gmail.com>
2016-04-19 20:52:01 -0700

    add multithread main

--- profiler/main.cpp
+++ profiler/main.cpp
@@ -4,8 +4,11 @@
 #include <stdint.h>
 
 #include <cstdio>
+#include <thread>
 
-void foo() {
+using namespace art_profiler;
+
+void f() {
     int64_t sum = 0;
     int j = 0;
     for (int i = 0; i < 100000000; ++i) {
@@ -15,19 +18,56 @@ void foo() {
     printf("sum = %" PRId64 " \n", sum);
 }
 
-int main() {
+void f0() {
+    Prof prof(0);
+    f();
+}
+
+void f1() {
+    Prof prof(1);
+    f();
+}
+
+void f2() {
+    Prof prof(2);
+    f();
+}
+
+void SingleThreadTest() {
     using namespace art_profiler;
     ProfMgr mgr(ProfMgr::SINGLE_THREAD, 1);
     mgr.add_tag_name(0, "TEST");
     {
         Prof prof("foo");
-        foo();
+        f();
     }
     {
         Prof prof("foo");
-        foo();
+        f();
     }
     mgr.Report();
+}
+
+void MultiThreadTest() {
+    ProfMgr mgr(ProfMgr::MULTI_THREAD, 3);
+    mgr.add_tag_name(0, "A");
+    mgr.add_tag_name(1, "AA");
+    mgr.add_tag_name(2, "AAA");
+
+    std::thread t0 {f0};
+    std::thread t1 {f1};
+    std::thread t01 {f0};
+
+    t0.join();
+    t1.join();
+    t01.join();
+
+    mgr.Report();
+}
+
+int main() {
+    // SingleThreadTest()
+    MultiThreadTest();
 
     return 0;
 }
